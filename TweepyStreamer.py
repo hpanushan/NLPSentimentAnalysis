cbc27939a40108f3ca467e1b6a1f2ce8e52c9fcc
anushanramesh <anushan@globalwavenet.com>
2019-06-25 13:21:43 +0530

    Added tweepy streamer files into the folder

---
+++ TweepyStreamer.py
@@ -0,0 +1,23 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+import Credentials
+
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print(data)
+        return True
+    
+    def on_error(self, status):
+        print(status)
+
+if __name__ == "__main__":
+    listener = StdOutListener()
+    auth = OAuthHandler(Credentials.CONSUMER_KEY, Credentials.CONSUMER_KEY_SECRET)
+    auth.set_access_token(Credentials.ACCESS_TOKEN, Credentials.ACCESS_TOKEN_SECRET)
+
+    stream = Stream(auth, listener)
+    ## Filtering the tweets
+    stream.filter(track = ["microsoft"])
