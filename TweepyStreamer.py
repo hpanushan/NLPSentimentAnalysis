b7b1b1a4e6e2e90b0529cb68f6024c28f1fe5651
anushanramesh <anushan@globalwavenet.com>
2019-06-25 13:22:42 +0530

    Converted string type into dictionary data type in live streaming code

--- TweepyStreamer.py
+++ TweepyStreamer.py
@@ -1,13 +1,15 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+import json
 
 import Credentials
 
 class StdOutListener(StreamListener):
 
     def on_data(self, data):
-        print(data)
+        dictData = json.loads(data)
+        print(dictData['id'])
         return True
     
     def on_error(self, status):
@@ -20,4 +22,4 @@ if __name__ == "__main__":
 
     stream = Stream(auth, listener)
     ## Filtering the tweets
-    stream.filter(track = ["microsoft"])
+    stream.filter(track = ["donald"])
