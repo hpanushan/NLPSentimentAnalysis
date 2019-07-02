47469e1ad6a04da9655e2a87c70ee811300f48f7
anushanramesh <anushan@globalwavenet.com>
2019-07-02 11:55:13 +0530

    Finished seperating to the streaming and storing them into mysql

--- Streaming_and_Storing_Data/TweepyStreamer.py
+++ Streaming_and_Storing_Data/TweepyStreamer.py
@@ -1,6 +1,5 @@
-from tweepy.streaming import StreamListener
-from tweepy import OAuthHandler
-from tweepy import Stream
+from __future__ import absolute_import, print_function
+from tweepy import OAuthHandler, Stream, StreamListener
 from StoringMySQL import storeMYSQL
 import json
 
@@ -9,17 +8,15 @@ import Credentials
 class StdOutListener(StreamListener):
 
     def on_data(self, data):
-        dictData = json.loads(data)                 # Convert string into dictionary
-        storeMYSQL(dictData)                        # Adding data to MySQL database
+        dict_data = json.loads(data)
+        storeMYSQL(dict_data)
         return True
-    
+
     def on_error(self, status):
         print(status)
 
     def streamer(self):
         auth = OAuthHandler(Credentials.CONSUMER_KEY, Credentials.CONSUMER_KEY_SECRET)
         auth.set_access_token(Credentials.ACCESS_TOKEN, Credentials.ACCESS_TOKEN_SECRET)
-
         stream = Stream(auth, self)
-        ## Filtering the tweets
-        stream.filter(track = ["donald trump"])
+        stream.filter(track=['obama'])
---
+++ Streaming_and_Storing_Data/main.py
@@ -0,0 +1,10 @@
+
+from TweepyStreamer import StdOutListener
+
+def main():
+    obj = StdOutListener()
+    obj.streamer()
+
+if __name__ == "__main__": 
+    # calling main function 
+    main()
